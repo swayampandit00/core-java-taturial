7d553f4b31fb18eaf042876b913ac11296911a5f
Swayam upadhyay <149935246+swayampandit00@users.noreply.github.com>
2026-03-20 16:05:50 +0530

    Add files via upload

---
+++ basic/TypeConversion.java
@@ -0,0 +1,15 @@
+public class TypeConversion {
+    public static void main(String[] args) {
+        float myFloat = 5;
+        System.out.println(myFloat);
+
+        int myInt = (int) 5.45f;
+        System.out.println(myInt);
+    }
+}
+
+
+// output
+/*5.0
+5
+*/
