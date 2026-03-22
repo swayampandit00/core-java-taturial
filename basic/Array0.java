808b9818a4b904dcc5b89c9685e55909043aec02
Swayam upadhyay <149935246+swayampandit00@users.noreply.github.com>
2026-03-22 12:48:56 +0530

    Add files via upload
    loop

---
+++ basic/Array0.java
@@ -0,0 +1,27 @@
+public class Array0 {
+    public static void main(String[] args) {
+        int[] myArr = new int[5];
+        myArr[0] = 98;
+        myArr[3] = 65;
+        myArr[1] = 2;
+        myArr[2] = 8;
+        myArr[4] = 37;
+
+        int index = 2;
+
+        System.out.println(myArr[0]);
+        System.out.println(myArr[1]);
+        System.out.println(myArr[index]);
+        System.out.println(myArr[3]);
+        System.out.println(myArr[4]);
+
+        
+       
+    }
+}
+
+
+
+
+
+
---
+++ basic/Array1.java
@@ -0,0 +1,27 @@
+public class Array1 {
+    public static void main(String[] args) {
+
+        int[] myArr = {98, 2, 8, 65, 37};
+
+        // Array Traversal
+        int index = 0;
+        while (index < myArr.length) {
+            System.out.println(myArr[index]);
+            index++;
+        }
+
+
+       String[] strArr = new String[4];
+        strArr[0] = "My String";
+
+        String[] newStrArr = {"first", "second", "third"};
+        System.out.print("the array length is=");
+        System.out.println(newStrArr.length);
+    }
+}
+
+
+
+
+
+
